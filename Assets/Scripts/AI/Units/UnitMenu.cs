06c96408f45acdeea53b36abbe68941d755e970f
Alex Roth <me@magmastone.net>
2013-11-15 13:31:47 -0500

    Started working on menu for each unit

---
+++ Assets/Scripts/AI/Units/UnitMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(1)){
+		GameObject.Find("RTSCamera").GetComponent<MenuFollower>().setTarget(transform);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Camera Control/MenuFollower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFollower : MonoBehaviour {
+	public Transform tofollow;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void setTarget(Transform tran){
+		tofollow = tran;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		Vector3 vec = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(tofollow.position);
+		GUI.Box(new Rect(vec.x,vec.y,100,90), "Unit Menu");
+	}
+}

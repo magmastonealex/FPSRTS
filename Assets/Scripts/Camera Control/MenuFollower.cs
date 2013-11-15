acb12ec38b349acaab8c2437b0fc9257421150df
Alex Roth <me@magmastone.net>
2013-11-15 16:16:51 -0500

    Got the MenuFollower to actually... follow.

--- Assets/Scripts/Camera Control/MenuFollower.cs
+++ Assets/Scripts/Camera Control/MenuFollower.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class MenuFollower : MonoBehaviour {
 	public Transform tofollow;
+	public bool menuShowing;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	public void setTarget(Transform tran){
+		menuShowing=true;
 		tofollow = tran;
 	}
 	// Update is called once per frame
@@ -15,7 +17,12 @@ public class MenuFollower : MonoBehaviour {
 	
 	}
 	void OnGUI () {
-		Vector3 vec = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(tofollow.position);
-		GUI.Box(new Rect(vec.x,vec.y,100,90), "Unit Menu");
+		if(menuShowing){
+			Vector3 vec = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(tofollow.position);
+			if(vec.x > 0 && vec.y >0){
+				Debug.Log(vec.y);
+				GUI.Box(new Rect(vec.x,Screen.height-vec.y,100,90), "Unit Menu");
+			}
+		}
 	}
 }

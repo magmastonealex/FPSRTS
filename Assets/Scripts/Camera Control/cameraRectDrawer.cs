01c2e5515c68838b5a1a022edb07887989624a4c
Alex Roth <me@magmastone.net>
2013-11-18 13:41:42 -0500

    Working on mini-map
    Started on camera rectangle.

--- Assets/Scripts/Camera Control/cameraRectDrawer.cs
+++ Assets/Scripts/Camera Control/cameraRectDrawer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class cameraRectDrawer : MonoBehaviour {
 	public Transform camera;
+	public GUIStyle st;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,7 +13,7 @@ public class cameraRectDrawer : MonoBehaviour {
 	void OnGUI () {
 	Vector3 vec = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(camera.position);
 
-	GUI.Box(new Rect(vec.x-80,(Screen.height-vec.y)-80,100,90), "");
+	GUI.Box(new Rect(vec.x-80,(Screen.height-vec.y)-80,100,90), "", st);
 	
 	}
 }

86521c0748b4a678a09418c262f12af0706ee333
Alex Roth <me@magmastone.net>
2013-11-18 14:39:50 -0500

    Mini-map - drawing rectangle for view

--- Assets/Scripts/Camera Control/cameraRectDrawer.cs
+++ Assets/Scripts/Camera Control/cameraRectDrawer.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 
 public class cameraRectDrawer : MonoBehaviour {
-	public Transform camera;
 	public GUIStyle st;
+	getCameraExtents ce;
+	public Rect boundingBox;
+	public GameObject marker;
 	// Use this for initialization
 	void Start () {
-	
+		ce = this.gameObject.GetComponent<getCameraExtents> ();
 	}
 	
 	// Update is called once per frame
 	void OnGUI () {
-	Vector3 vec = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(camera.position);
-
-	GUI.Box(new Rect(vec.x-80,(Screen.height-vec.y)-80,100,90), "", st);
+	Vector3 vectr = this.gameObject.GetComponent<Camera>().WorldToScreenPoint(ce.cameraTopRight);
+	Vector3 vecbl = this.gameObject.GetComponent<Camera> ().WorldToScreenPoint(ce.cameraBottomLeft);
+	Vector3 vectl = this.gameObject.GetComponent<Camera> ().WorldToScreenPoint(ce.cameraTopLeft);
+	Vector3 vecmk = this.gameObject.GetComponent<Camera> ().WorldToScreenPoint(marker.transform.position);
+		boundingBox = new Rect (vecmk.x-vectl.x, (Screen.height-vecmk.y), 10, 10);
+		GUI.Box(boundingBox, "", st);
 	
 	}
 }
---
+++ Assets/Scripts/Camera Control/getCameraExtents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getCameraExtents : MonoBehaviour {
+	public Camera cameraForExtents;
+	public GameObject referenceObject;
+	public Vector3 cameraTopLeft;
+	public Vector3 cameraTopRight;
+	public Vector3 cameraBottomLeft;
+	public Vector3 cameraBottomRight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float dist = Vector3.Distance(cameraForExtents.transform.position, referenceObject.transform.position);
+		cameraBottomLeft = cameraForExtents.ScreenToWorldPoint(new Vector3 (0, 0,dist));
+		cameraBottomRight = cameraForExtents.ScreenToWorldPoint(new Vector3 (Screen.width-1, 0,dist));
+		cameraTopLeft = cameraForExtents.ScreenToWorldPoint(new Vector3 (0, Screen.height-1,dist));
+		cameraTopRight = cameraForExtents.ScreenToWorldPoint(new Vector3 (Screen.width-1, Screen.height-1,dist));
+	}
+}

c528804ce8b2bf227cc7a76031189907db58cf62
Alex Roth <me@magmastone.net>
2013-11-21 19:45:59 -0500

    added some beginnings to the login/DRM algo. Working further on pathfinding.

---
+++ Assets/Scripts/ManagementScripts/getMAC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System;
+public class getMAC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Debug.Log(GetCrypt(SystemInfo.deviceUniqueIdentifier+SystemInfo.graphicsDeviceVendor+SystemInfo.graphicsDeviceVersion+SystemInfo.graphicsDeviceName+SystemInfo.processorType));
+	}
+
+	public static string GetCrypt(string text)
+	{
+		string hash = "";
+		SHA512 alg = SHA512.Create();
+		byte[] result = alg.ComputeHash(Encoding.UTF8.GetBytes(text));
+		return Convert.ToBase64String(result);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
